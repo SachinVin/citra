bc7831749af0766ef68538526393dc6fce05ca97
SachinVin <sachinvinayak2000@gmail.com>
2021-04-09 23:41:34 +0530

    m pwd

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -8,5 +8,5 @@ brew install qt5 sdl2 p7zip ccache ffmpeg llvm ninja || true
 pip3 install macpack
 wget https://homebrew.bintray.com/bottles/qt%405-5.15.2.arm64_big_sur.bottle.tar.gz
 tar -xzf qt@5-5.15.2.arm64_big_sur.bottle.tar.gz
-
+pwd
 
