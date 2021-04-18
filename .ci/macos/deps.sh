847645ffdedede51c4cacc6c6a07bca540810505
SachinVin <sachinvinayak2000@gmail.com>
2021-04-18 20:43:08 +0530

    m17

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -7,8 +7,8 @@ brew options sdl2
 # brew install qt5 sdl2 p7zip ccache ffmpeg llvm ninja || true
 brew install p7zip ccache ffmpeg llvm ninja || true
 pip3 install macpack
-wget https://homebrew.bintray.com/bottles/qt%405-5.15.2.arm64_big_sur.bottle.tar.gz
-tar -xzf qt@5-5.15.2.arm64_big_sur.bottle.tar.gz
+#wget https://homebrew.bintray.com/bottles/qt%405-5.15.2.arm64_big_sur.bottle.tar.gz
+#tar -xzf qt@5-5.15.2.arm64_big_sur.bottle.tar.gz
 wget https://homebrew.bintray.com/bottles/sdl2-2.0.14_1.arm64_big_sur.bottle.tar.gz
 tar -xzf sdl2-2.0.14_1.arm64_big_sur.bottle.tar.gz
 
